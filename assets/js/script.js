d530e7a14f2f47115ca1944654a871aab39bf05e
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-07 16:59:15 -0500

    pinting out daily list.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,16 @@ var key = "168391797cc48918fbec2db27de39874";
 var searchBarListEL = $("#searchBarList");
 var searchBarEL = $("#searchBar");
 var searchBtnEL = $("#searchBtn");
+var weatherTodayEl = $("#weatherToday");
+// var weatherTodayUL = $("#weatherTodayUL");
 
 //****************************Get weather data***************** */
-function getWeather(lat, lon) {
+function getWeather(lat, lon, name) {
   var part = "alerts";
   var unit = "imperial";
+  var name = "Seattle";
+  var lat = "47.6062";
+  var lon = "-122.332";
   fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&units=${unit}&appid=${key}`
   )
@@ -17,40 +22,84 @@ function getWeather(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      console.log("first call");
+      weatherToday(data);
     });
+  // .catch(function (error) {
+  //   console.error(error);
+  // });
 }
 // ***********************************get location data***********************
-function getLocation(searchVal) {
-  const settings = {
-    async: true,
-    crossDomain: true,
-    url: `https://spott.p.rapidapi.com/places/autocomplete?limit=5&skip=0&country=US%2CCA&q=${searchVal}&type=CITY`,
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "spott.p.rapidapi.com",
-      "x-rapidapi-key": "4358a3ae45msh1ef514db96f084bp1426f0jsn143dce70b436",
-    },
-  };
+// function getLocation(searchVal) {
+//   const settings = {
+//     async: true,
+//     crossDomain: true,
+//     url: `https://spott.p.rapidapi.com/places/autocomplete?limit=5&skip=0&country=US%2CCA&q=${searchVal}&type=CITY`,
+//     method: "GET",
+//     headers: {
+//       "x-rapidapi-host": "spott.p.rapidapi.com",
+//       "x-rapidapi-key": "4358a3ae45msh1ef514db96f084bp1426f0jsn143dce70b436",
+//     },
+//   };
 
-  $.getJSON(settings, function (data) {
-    try {
-      console.log(data);
-      var timeZone = data[0].timezoneId;
-      var name = data[0].name;
-      var lat = data[0].coordinates.latitude;
-      var lon = data[0].coordinates.longitude;
-      console.log(lat);
-      console.log(lon);
-      console.log(name);
-      console.log(timeZone);
-      getWeather(lat, lon);
-    } catch {
-      console.log("no city found");
-      searchBarEL.text = "";
-    }
+//   $.getJSON(settings, function (data) {
+//     try {
+//       console.log(data);
+//       var timeZone = data[0].timezoneId;
+//       var name = data[0].name;
+//       var lat = data[0].coordinates.latitude;
+//       var lon = data[0].coordinates.longitude;
+//       console.log(lat);
+//       console.log(lon);
+//       console.log(name);
+//       console.log(timeZone);
+//       getWeather(lat, lon, name);
+//     } catch {
+//       console.log("no city found");
+//       searchBarEL.text = "";
+//     }
+//   });
+// }
+
+// *****************************show todays forecast in main window***************
+
+//pull info about today's weather
+function weatherToday(data) {
+  var today = data.daily[0];
+  var todayMaxTemp = "Maximum Temperature:  " + data.daily[0].temp.max + " ᵒF";
+  var todayMinTemp = "Minimum Temperature:  " + data.daily[0].temp.min + " ᵒF";
+  var todayWindSpeed = "Wind Speed:  " + data.daily[0].wind_speed + "MPH";
+  var todayHumidity = data.daily[0].humidity;
+  var todayUVIndex = data.daily[0].uvi;
+  var todayWeatherType = "Today we will have: " + data.daily[0].weather[0].main;
+  var todayWeatherIcon = data.daily[0].weather[0].icon;
+  //put in array to loop through
+  var todayList = [
+    todayWeatherType,
+    todayMaxTemp,
+    todayMinTemp,
+    todayWindSpeed,
+    todayHumidity,
+    todayUVIndex,
+  ];
+  console.log(data);
+  console.log(today);
+  console.log(todayList);
+  console.log("max temp = " + todayMaxTemp);
+  console.log("min temp = " + todayMinTemp);
+  console.log("wind speed = " + todayWindSpeed);
+  console.log("humidity = " + todayHumidity);
+  console.log("UV index = " + todayUVIndex);
+  console.log("Weather description = " + todayWeatherType);
+  console.log("Weather icon = " + todayWeatherIcon);
+
+  $.each(todayList, function (i, val) {
+    console.log(val);
+    var weatherTodayLI = $("<li>").text(val).attr("class", "weatherTodayLI");
+    $("#weatherTodayUL").append(weatherTodayLI);
   });
 }
+
 // *****************************search history*********************
 var pastSearches = [];
 
@@ -88,7 +137,7 @@ historyOnLoad();
 function historyOnLoad(e) {
   try {
     drawPastSearches();
-    console.log("post past searches");
+    console.log("post searches");
   } catch {
     $("#searchHistoryLabel").css("display", "none");
     console.log("no past searches");
@@ -101,7 +150,8 @@ function historyOnLoad(e) {
 $(searchBtnEL).on("click", function (e) {
   var searchVal = searchBar.value;
   searchBarListEL.empty();
-  getLocation(searchVal);
+  // getLocation(searchVal);
+  getWeather();
   saveSearchHistory(searchVal);
 });
 //on enter key press while in search bar run get location function
@@ -110,7 +160,8 @@ $(searchBarEL).on("keyup", function (e) {
     var searchVal = searchBar.value;
     searchBarListEL.empty();
     console.log(searchVal);
-    getLocation(searchVal);
+    // getLocation(searchVal);
+    getWeather();
     saveSearchHistory(searchVal);
   }
 });
@@ -119,5 +170,6 @@ $(searchBarListEL).on("click", function (e) {
   var textValue = e.target.innerText;
   console.log(textValue);
   var searchVal = textValue;
-  getLocation(searchVal);
+  // getLocation(searchVal);
+  getWeather();
 });
