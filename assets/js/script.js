d64ecc9a94320a1881dfd678ebddb578412691a7
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-07 13:32:15 -0500

    basic grid mostly worked out.

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,29 @@ function getWeather() {
       console.log(data);
     });
 }
+
+getLocation();
+
+//*****************location APi */
+function getLocation() {
+  var city = "atlanta";
+  fetch(
+    `https://spott.p.rapidapi.com/places/autocomplete?limit=10&skip=0&country=US%2CCA&q=${city}&type=CITY`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "spott.p.rapidapi.com",
+        "x-rapidapi-key": "4358a3ae45msh1ef514db96f084bp1426f0jsn143dce70b436",
+      },
+    }
+  )
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("city data" + data);
+    });
+}
